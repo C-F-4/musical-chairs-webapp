51eb094867fa843a8bd1800cf6d614c52cbbd8c3
C-F-4 <abhinavsaxena67@gmail.com>
2021-03-22 21:24:19 +0530

    feat(screen): Profile Support

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { IUser } from '../interfaces';
+import { UserService } from '../services';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: IUser;
+  formProfile!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {
+    this.user = this.userService.loggedUser;
+  }
 
   ngOnInit(): void {
+    this.formProfile = this.fb.group({
+      email: [this.user.email, Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      firstname: [this.user.firstname, Validators.compose([
+        Validators.required
+      ])],
+      lastname: [this.user.lastname, Validators.compose([
+        Validators.required
+      ])],
+      pass: [this.user.password, Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(16)
+      ])]
+    });
+  }
+
+  get avatar(): string {
+    return this.user.avatar;
+  }
+
+  get username(): string {
+    return this.user.username;
+  }
+
+  onUpdateProfile(evt: Event): void {
+    // 
   }
 
 }
